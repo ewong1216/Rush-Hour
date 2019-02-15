8d6f31223c9eb162b7bfcd6cc46ac0c84023e777
s-wonget <s-wonget@51L104-14.it.bsd405.org>
2019-02-15 09:47:18 -0800

    Imported to desktop

--- RushHour/src/PuzzleBoard.java
+++ RushHour/src/PuzzleBoard.java
@@ -1,66 +1,57 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class PuzzleBoard
-{
+public class PuzzleBoard{
 	// Do not change the name or type of this field
 	private Vehicle[] idToVehicle;
 	
 	// You may add additional private fields here
 	
-	public PuzzleBoard(Vehicle[] idToVehicleP)
-	{
-		throw new UnsupportedOperationException();
+	public PuzzleBoard(Vehicle[] idToVehicleP){
+		idToVehicle = new Vehicle[idToVehicleP.length];
+		for(int i = 0; i < idToVehicleP.length; i++){
+			if(idToVehicleP[i] != null){
+				idToVehicle[i] = idToVehicleP[i];
+			}
+		}
 	}
 	
-	public Vehicle getVehicle(int id)
-	{
+	public Vehicle getVehicle(int id){
 		throw new UnsupportedOperationException();
 	}
 
-	public Vehicle getVehicle(int row, int column)
-	{
+	public Vehicle getVehicle(int row, int column){
 		throw new UnsupportedOperationException();
 	}
 	
-	public int heuristicCostToGoal()
-	{
+	public int heuristicCostToGoal(){
 		throw new UnsupportedOperationException();
 	}
 	
-	public boolean isGoal()
-	{
+	public boolean isGoal(){
 		throw new UnsupportedOperationException();
 	}
 	
-	public Iterable<PuzzleBoard> getNeighbors()
-	{
+	public Iterable<PuzzleBoard> getNeighbors(){
 		throw new UnsupportedOperationException();
 	}
 	
-	@Override
-	public String toString()
-	{
+	public String toString(){
 		// You do not need to modify this code, but you can if you really
 		// want to.  The automated tests will not use this method, but
 		// you may find it useful when testing within Eclipse
 		
 		String ret = "";
-		for (int row=0; row < PuzzleManager.NUM_ROWS; row++)
-		{
-			for (int col=0; col < PuzzleManager.NUM_COLUMNS; col++)
-			{
+		for (int row=0; row < PuzzleManager.NUM_ROWS; row++){
+			for (int col=0; col < PuzzleManager.NUM_COLUMNS; col++){
 				Vehicle vehicle = getVehicle(row, col);
-				if (vehicle == null)
-				{
+				if (vehicle == null){
 					ret += " . ";
 				}
-				else
-				{
+				else{
 					int id = vehicle.getId(); 
 					ret += " " + id;
-					if (id < 10)
-					{
+					if (id < 10){
 						ret += " ";
 					}
 				}
@@ -68,11 +59,9 @@ public class PuzzleBoard
 			ret += "\n";
 		}
 		
-		for (int id = 0; id < PuzzleManager.MAX_NUM_VEHICLES; id++)
-		{
+		for (int id = 0; id < PuzzleManager.MAX_NUM_VEHICLES; id++){
 			Vehicle v = getVehicle(id);
-			if (v != null)
-			{
+			if (v != null){
 				ret += "id " + v.getId() + ": " + 
 						(v.getIsHorizontal() ? "h (" : "v (") + 
 						v.getLeftTopRow() + "," + v.getLeftTopColumn() + "), " + v.getLength() + "  \n";
@@ -82,9 +71,7 @@ public class PuzzleBoard
 		return ret;
 	}
 	
-	@Override
-	public int hashCode()
-	{
+	public int hashCode(){
 		// DO NOT MODIFY THIS METHOD
 		
 		final int prime = 31;
@@ -93,29 +80,23 @@ public class PuzzleBoard
 		return result;
 	}
 
-	@Override
-	public boolean equals(Object obj)
-	{
+	public boolean equals(Object obj){
 		// DO NOT MODIFY THIS METHOD
 		
-		if (this == obj)
-		{
+		if (this == obj){
 			return true;
 		}
 		
-		if (obj == null)
-		{
+		if (obj == null){
 			return false;
 		}
 		
-		if (getClass() != obj.getClass())
-		{
+		if (getClass() != obj.getClass()){
 			return false;
 		}
 		
 		PuzzleBoard other = (PuzzleBoard) obj;
-		if (!Arrays.equals(idToVehicle, other.idToVehicle))
-		{
+		if (!Arrays.equals(idToVehicle, other.idToVehicle)){
 			return false;
 		}
 		return true;
