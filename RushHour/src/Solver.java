47e31966b8e5eaac678bf4e598c4710a8efb19f4
s-wonget <s-wonget@51S50023594.it.bsd405.org>
2019-02-27 21:57:00 -0800

    CP 3

--- RushHour/src/Solver.java
+++ RushHour/src/Solver.java
@@ -2,23 +2,48 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class Solver
-{
-	private static class SearchNode implements Comparable<SearchNode>
-	{
+public class Solver{
+	private SearchNode goal;
+	
+	public Solver(PuzzleBoard initial){
+		UpdateableMinPQ<SearchNode> pq = new UpdateableMinPQ<SearchNode>();
+		pq.insert(new SearchNode(initial,0,null));
+
+		while(!pq.isEmpty()){
+			SearchNode cur = pq.delMin();
+			if(cur.board.isGoal()){
+				goal = cur;
+				break;
+			}
+			for(PuzzleBoard b : cur.board.getNeighbors()){
+				pq.insert(new SearchNode(b,cur.costFromBeginningToHere+1,cur));
+			}
+		}
+	}
+
+	private static class SearchNode implements Comparable<SearchNode>{
 		// Important!! Do not change the names or types of these fields!
 		private PuzzleBoard board;
 		private int costFromBeginningToHere;
 		private SearchNode previous;
-
-		public int compareTo(SearchNode that)
-		{
-			throw new UnsupportedOperationException();
+		public SearchNode(PuzzleBoard b, int c, SearchNode p){
+			board = b;
+			costFromBeginningToHere = c;
+			previous = p;
+		}
+		public int compareTo(SearchNode that){
+			int cost = costFromBeginningToHere + board.heuristicCostToGoal();
+			int otherCost = that.costFromBeginningToHere + that.board.heuristicCostToGoal();
+			if(cost < otherCost){
+				return -1;
+			}
+			if(cost == otherCost){
+				return 0;
+			}
+			return 1;
 		}
 
-		@Override
-		public int hashCode()
-		{
+		public int hashCode(){
 			// DO NOT MODIFY THIS METHOD
 
 			final int prime = 31;
@@ -29,68 +54,55 @@ public class Solver
 			return result;
 		}
 
-		@Override
-		public boolean equals(Object obj)
-		{
+		public boolean equals(Object obj){
 			// DO NOT MODIFY THIS METHOD
 
-			if (this == obj)
-			{
+			if (this == obj){
 				return true;
 			}
-			if (obj == null)
-			{
+			if (obj == null){
 				return false;
 			}
-			if (getClass() != obj.getClass())
-			{
+			if (getClass() != obj.getClass()){
 				return false;
 			}
 			SearchNode other = (SearchNode) obj;
-			if (board == null)
-			{
-				if (other.board != null)
-				{
+			if (board == null){
+				if (other.board != null){
 					return false;
 				}
 			} 
-			else if (!board.equals(other.board))
-			{
+			else if (!board.equals(other.board)){
 				return false;
 			}
-			if (costFromBeginningToHere != other.costFromBeginningToHere)
-			{
+			if (costFromBeginningToHere != other.costFromBeginningToHere){
 				return false;
 			}
-			if (previous == null)
-			{
-				if (other.previous != null)
-				{
+			if (previous == null){
+				if (other.previous != null){
 					return false;
 				}
 			}
-			else if (!previous.equals(other.previous))
-			{
+			else if (!previous.equals(other.previous)){
 				return false;
 			}
 			return true;
 		}
 	}
 
-	public Solver(PuzzleBoard initial)
-	{
-		throw new UnsupportedOperationException();
-	}
-
-	public Solver(PuzzleBoard initial, boolean extraCredit)
-	{
+	public Solver(PuzzleBoard initial, boolean extraCredit){
 		// DO NOT TOUCH unless you are passing all of the tests and wish to
 		// attempt the extra credit.
 		throw new UnsupportedOperationException();
 	}
 
-	public Iterable<PuzzleBoard> getPath()
-	{
-		throw new UnsupportedOperationException();
+	public Iterable<PuzzleBoard> getPath(){
+		Stack<PuzzleBoard> s = new Stack<PuzzleBoard>();
+		SearchNode cur = goal;
+		while(cur != null){
+			s.push(cur.board);
+			cur = cur.previous;
+		}
+		return s;
 	}
 }
